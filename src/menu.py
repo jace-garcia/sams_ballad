81e4d6b7b049e1b7bdbc4b9ca7f7c2384127435e
jace-garcia <jcgarcia@mit.edu>
2020-09-17 13:39:36 -0400

    got start click to load chapter select, todo: link selected chapter and game, and clean up chapter select visual

--- src/menu.py
+++ src/menu.py
@@ -23,65 +23,105 @@ class MenuWidget(BaseWidget):
         self.display.on_layout(win_size)
 
     def on_update(self):
-        pass
+        self.display.on_update()
 
 class MenuDisplay(InstructionGroup):
-    def __init__(self):
+    def __init__(self, num_chapters=5):
         super(MenuDisplay, self).__init__()
+        w, h = (Window.width, Window.height)
         self.background_src = '../data/img/start_screen/background.png'
         self.title_src = '../data/img/text/sams_ballad.png'
         self.start = None
-        self.on_layout((Window.width, Window.height))
-
+        self.chapter_select = None
+        self.num_ch = num_chapters
         self.load_chapter_select = False
         self.load_start = True
+        self.start_clicked = False
+        self.on_layout((w, h))
+
+        self.ch_size = (w * 1/10, h/6)
+        self.num_size = (w * 1/15, h/6)
+        self.btwn_size = w * 1/10
+        self.init_pos = (w * 1/2, h/4)
 
     def on_start_click(self):
-        print('start clicked')
         self.load_start = False
         self.load_chapter_select = True
+        self.start_clicked = True
 
     def on_touch_down(self, touch):
-        self.start.on_touch_down(touch)
+        if self.load_start:
+            self.start.on_touch_down(touch)
     
     def on_layout(self, win_size):
-        print(win_size)
         self.clear()
         w, h = win_size
 
         background_img = Rectangle(source=self.background_src, pos=(0,0), size=(w, h))
         self.add(background_img)
 
-        self.start = StartButton(on_click=self.on_start_click)
-        self.add(self.start)
-
         title_pos = (w * (1/4), h * (7/10))
         title_size = (w * (7/10), h / 5)
         title = Rectangle(source=self.title_src, pos=title_pos, size=title_size)
         self.add(title)
 
+        if self.load_start:
+            self.start = StartButton(on_click=self.on_start_click)
+            self.add(self.start)
+        if self.load_chapter_select:
+            self.load_chapters(win_size)
+
+    def load_chapters(self, win_size):
+        w, h = win_size
+        ch_size = self.ch_size
+        num_size = self.num_size
+        current_pos = self.init_pos
+        
+        icon_width = ch_size[0] + num_size[0] + self.btwn_size
+        icon_height = ch_size[1]
+
+        for i in range(self.num_ch):
+            pos = ()
+            if i % 2 == 0: # newline
+                pos = (current_pos[0], current_pos[1] - icon_height * (i/2))
+                current_pos = (current_pos[0], pos[1])
+            else: # adjacent
+                pos = (current_pos[0] + icon_width * (i%2), current_pos[1])
+            self.add(ChapterDisplay(chapter_num=i+1, pos=pos, ch_size=self.ch_size, num_size=self.num_size, btwn_size=self.btwn_size, active=True))
+
+
     def on_update(self):
-        pass
+        if self.start_clicked:
+            self.start_clicked = False
+            self.on_layout((Window.width, Window.height))
+        
 
 class ChapterDisplay(InstructionGroup):
-    def __init__(self, chapter_num):
+    def __init__(self, chapter_num, pos, ch_size, num_size, btwn_size, active=True):
         super(ChapterDisplay, self).__init__()
         self.ch_img = '../data/img/text/ch.png'
         self.num_img = '../data/img/text/' + str(chapter_num) + '.png'
         self.boundaries = []
+        self.pos = pos
+        self.chapter_num = chapter_num
+        self.ch_size = ch_size
+        self.num_size = num_size
+        self.btwn_size = btwn_size
+
+        self.on_layout((Window.width, Window.height))
 
     def on_layout(self, win_size):
         self.clear()
 
         w, h = win_size
-        ch_pos = (w * 1/2, h/4)
-        ch_size = (w * 1/10, h/6)
+        ch_pos = self.pos
+        ch_size = self.ch_size
         self.add(Rectangle(source=self.ch_img, pos=ch_pos, size=ch_size))
-        num_pos = (w * 1/2 + w * 1/16, h/4)
-        num_size = (w * 1/15, h/6)
+        num_pos = (ch_pos[0] + self.btwn_size, ch_pos[1])
+        num_size = self.num_size
         self.add(Rectangle(source=self.num_img, pos=num_pos, size=num_size))
-
-        self.boundaries = [(ch_pos[0], ch_pos[0] + ch_size[0] + num_size[0]), (ch_pos[1], ch_pos[1] + ch_size[1])]
+        # [(x1, x2), (y1, y2)]
+        self.boundaries = [(ch_pos[0], ch_pos[0] + ch_size[0] + num_size[0] + w * 1/16), (ch_pos[1], ch_pos[1] + ch_size[1])]
 
 class StartButton(InstructionGroup):
     def __init__(self, on_click = None):
